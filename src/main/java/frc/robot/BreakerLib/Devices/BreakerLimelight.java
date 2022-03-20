fdcea5a2be34064c2ab5f51c889b1282c46d23ec
Roman-Tech-Plus <66585473+Roman-Tech-Plus@users.noreply.github.com>
2022-03-19 20:15:02 -0700

    Update BreakerLimelight.java

--- src/main/java/frc/robot/BreakerLib/Devices/BreakerLimelight.java
+++ src/main/java/frc/robot/BreakerLib/Devices/BreakerLimelight.java
@@ -4,6 +4,8 @@
 
 package frc.robot.BreakerLib.Devices;
 
+import java.lang.annotation.Retention;
+
 import org.opencv.osgi.OpenCVInterface;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -15,23 +17,32 @@ public class BreakerLimelight extends SubsystemBase {
   private double targetHeight;
   private String limelightName;
   private LimelightTarget currentTarget;
-  /** Creates a new BreakerLimelight. */
+  /** Creates an new vision prossesing limelight
+   * @param limelightName the network name of the limelight you are initializing
+   */
   public BreakerLimelight(String limelightName) {
     limelightName = this.limelightName;
   }
 
+  /** sets the limelights mounting position relative to the ground
+   * @param mountingAngle the angle of the center of the limelights vision retive to the ground
+   * @param mountingHeight the height that the center of the limelight camera is mounted in relative to the ground
+  */
   public void setMountingPosition(double mountingAngle, double mountingHeight) {
     mountingAngle = this.mountingAngle;
     mountingAngle = this.mountingHeight;
   }
 
+  /** Returns the number of limelight's the active pipeline (0-9) */
   public double getPipeline() {
     return NetworkTableInstance.getDefault().getTable(limelightName).getEntry("getPipe").getDouble(0);
   }
 
+
   public void setPipeline(double pipeline) {
     NetworkTableInstance.getDefault().getTable(limelightName).getEntry("pipeline").setNumber(pipeline);
   }
+
 
   public String getName() {
     return limelightName;
@@ -46,15 +57,51 @@ public class BreakerLimelight extends SubsystemBase {
     }
   }
 
-  /** WIP */
+  /** Returns an array with all single vision values the limelight can return
+   * @return tv, tx, ty, ta, ts, tl, tshort, tlong, thor, tvert, getpipe, tx0, ty0, ta0, ts0, tx1, ty1, ta1, ts1, tx2, ty2, ta2, ts2
+   */
   public double[] getAllVisionData() {
-    double[] info = new double[28];
-    info[0] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("").getDouble(0);
+    double[] info = new double[26];
+    info[0] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tv").getDouble(0);
+    info[1] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tx").getDouble(0);
+    info[2] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ty").getDouble(0);
+    info[3] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ta").getDouble(0);
+    info[4] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ts").getDouble(0);
+    info[5] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tl").getDouble(0);
+    info[6] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tshort").getDouble(0);
+    info[7] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tlong").getDouble(0);
+    info[8] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("thor").getDouble(0);
+    info[9] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tvert").getDouble(0);
+    info[10] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("getpipe").getDouble(0);
+    info[11] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tx0").getDouble(0);
+    info[12] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ty0").getDouble(0);
+    info[13] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ta0").getDouble(0);
+    info[14] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ts0").getDouble(0);
+    info[15] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tx1").getDouble(0);
+    info[16] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ty1").getDouble(0);
+    info[17] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ta1").getDouble(0);
+    info[18] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ts1").getDouble(0);
+    info[19] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("tx2").getDouble(0);
+    info[20] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ty2").getDouble(0);
+    info[21] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ta2").getDouble(0);
+    info[22] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("ts2").getDouble(0);
+    info[23] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("cx0").getDouble(0);
+    info[24] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("cy0").getDouble(0);
+    info[25] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("cx1").getDouble(0);
+    info[26] = NetworkTableInstance.getDefault().getTable(limelightName).getEntry("cy1").getDouble(0);
     return info;
   }
 
   public LimelightTarget getCurrentTarget() {
     return currentTarget;
+  }
+
+  public double getMountingHeight() {
+      return mountingHeight;
+  }
+
+  public double getMountingAngle() {
+      return mountingAngle;
   }
 
   @Override
@@ -66,6 +113,11 @@ public class BreakerLimelight extends SubsystemBase {
     private int pipelineNum;
     private double targetHeight;
     private BreakerLimelight limelight;
+    /** Creates the profile for the target of a limelight
+     * @param targetHeight the height of the intended target form the ground
+     * @param limelight the limelight used to track the intended target
+     * @param pipelineNum the pipeline number (0 - 9) that the limelight needs to use to track the intended target
+     */
     public LimelightTarget(double targetHeight, BreakerLimelight limelight, double pipelineNum) {
       targetHeight = this.targetHeight;
       pipelineNum = this.pipelineNum;
@@ -84,9 +136,21 @@ public class BreakerLimelight extends SubsystemBase {
       return NetworkTableInstance.getDefault().getTable(limelight.getName()).getEntry("ty").getDouble(0);
     }
 
+    public double getTargetArea() {
+      return NetworkTableInstance.getDefault().getTable(limelight.getName()).getEntry("ta").getDouble(0);
+    }
+
+    public double getTargetSkew() {
+      return NetworkTableInstance.getDefault().getTable(limelight.getName()).getEntry("ts").getDouble(0);
+    }
+
+    public double get3DTargetData() {
+      return NetworkTableInstance.getDefault().getTable(limelight.getName()).getEntry("camtran").getDouble(0);
+    }
+
     public double getTargetDistance() {
-      double camHeight = Constants.HUB_HEIGHT_INS - Constants.SHOOT_CAM_HEIGHT;
-      double corTarAng = Constants.SHOOT_CAM_ANG + getTargetOffsetY();
+      double camHeight = targetHeight - limelight.getMountingHeight();
+      double corTarAng = limelight.getMountingAngle() + getTargetOffsetY();
       return (camHeight / Math.tan(corTarAng));
     }
   }
