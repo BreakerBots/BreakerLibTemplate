fa4b17a02d114e9fa4a6972f8e0508ec978efed2
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-03-20 10:23:20 -0700

    Part 2 of renaming

--- src/main/java/frc/robot/BreakerLib/subsystemcores/drivetrain/differential/BreakerWestCoastDrive.java
+++ src/main/java/frc/robot/BreakerLib/subsystemcores/drivetrain/differential/BreakerWestCoastDrive.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.BreakerLib.subsystemcores.drivetrain111.differential;
+package frc.robot.BreakerLib.subsystemcores.drivetrain.differential;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import frc.robot.BreakerLib.subsystemcores.drivetrain111.BreakerGenericDrivetrain;
+import frc.robot.BreakerLib.subsystemcores.drivetrain.BreakerGenericDrivetrain;
 
 public class BreakerWestCoastDrive extends BreakerGenericDrivetrain {
   private WPI_TalonFX leftLead;
--- src/main/java/frc/robot/BreakerLib/subsystemcores/drivetrain/differential/BreakerWestCoastDriveConfig.java
+++ src/main/java/frc/robot/BreakerLib/subsystemcores/drivetrain/differential/BreakerWestCoastDriveConfig.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.BreakerLib.subsystemcores.drivetrain111.differential;
+package frc.robot.BreakerLib.subsystemcores.drivetrain.differential;
 
 import frc.robot.BreakerLib.util.BreakerMath;
 
